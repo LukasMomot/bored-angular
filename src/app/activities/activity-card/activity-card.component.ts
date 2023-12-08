ecc4780dc263fc8f1ff9fe6df46db0dfe7d5fba2
Lukasz Momot <lukasz.momot@hotmail.com>
2023-12-08 14:26:48 +0100

    add suprise text component

--- src/app/activities/activity-card/activity-card.component.ts
+++ src/app/activities/activity-card/activity-card.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Activity } from '../activity';
 import { RouterLink } from '@angular/router';
+import { SupriseTextComponent } from "../suprise-text/suprise-text.component";
 
 @Component({
-  selector: 'app-activity-card',
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink
-  ],
-  templateUrl: './activity-card.component.html',
-  styleUrl: './activity-card.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-activity-card',
+    standalone: true,
+    templateUrl: './activity-card.component.html',
+    styleUrl: './activity-card.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        RouterLink,
+        SupriseTextComponent
+    ]
 })
 export class ActivityCardComponent {
   @Input( { required: true }) activity: Activity = {} as Activity;
---
+++ src/app/activities/suprise-text/suprise-text.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suprise-text',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  template: `<span>This is the best offer for you 😎</span>`,
+  styleUrl: './suprise-text.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SupriseTextComponent { }
